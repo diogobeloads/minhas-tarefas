ba49d10bca64631da893d49d90d46e1897efa4a6
Diogo Belo <diogobelodss22@gmail.com>
2025-07-13 12:55:12 -0300

    Criando variaveis para os estados e adicionando cores.

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,42 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
+import * as enums from '../../utils/enums/Tarefa'
+
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Pagar internet',
+    descricao: 'Valor devido',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
+  },
+  {
+    titulo: 'Estudar React',
+    descricao: 'Ver a aula 7',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  }
+]
 
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
---
+++ src/styles/variaveis.ts
@@ -0,0 +1,6 @@
+export default {
+  vermelho: '#ff0000ff',
+  verde: '#44bd32',
+  amarelo: '#f0c11a',
+  amarelo2: '#e1a32a'
+}
